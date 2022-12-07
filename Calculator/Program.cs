5d573ed88d61be48587737a0db385fad5979ef22
marquesmwagner <marquesmwagner@gmail.com>
2022-12-07 18:08:59 -0300

    Create Calculator Class and divide the code.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,42 +1,71 @@
-﻿double num1 = 0; double num2 = 0;
-
-Console.WriteLine("Console Calculator in C#\r");
-Console.WriteLine("------------------------\n");
-
-Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Type another number, and then press Enter");
-num2 = Convert.ToDouble(Console.ReadLine());
-
-Console.WriteLine("Choose an option from the following list:");
-Console.WriteLine("\ta - Add");
-Console.WriteLine("\ts - Subtract");
-Console.WriteLine("\tm - Multiply");
-Console.WriteLine("\td - Divide");
-Console.WriteLine("Your option? ");
-
-switch (Console.ReadLine())
+﻿namespace Calculator
 {
-    case "a":
-        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-        break;
-
-    case "s":
-        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-        break;
-    case "m":
-        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-        break;
-    case "d":
-        while (num2 == 0)
+    class Program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine("Enter a non-zero divisor: ");
-            num2 = Convert.ToDouble(Console.ReadLine());
-        }
-        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-        break;
-}
+            bool endApp = false;
+            
+            Console.WriteLine("Console Calculator in C#\n");
+            Console.WriteLine("------------------------\n");
 
-Console.WriteLine("Press any key to close the Calculator console app...");
-Console.ReadKey();
+            while (!endApp)
+            {
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                Console.WriteLine("Type a number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.WriteLine("This is not a valid input. Please enter an integer value: ");
+                    numInput1= Console.ReadLine();
+                }
+
+                Console.WriteLine("Type another number, and then press Enter : ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.WriteLine("This is not a valid input. Please enter an integer value: ");
+                    numInput2= Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("Your option?");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine("Oh no! An exception ocurred trying to do the math.\n - Details: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                Console.WriteLine("Press 'n' and Enter to close the app, or any other Key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n");
+            }
+            return;
+        }
+    }
+}
