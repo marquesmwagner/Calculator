33b0c67e0d42436e66124735728cc0c7649e4136
marquesmwagner <marquesmwagner@gmail.com>
2022-12-07 17:41:27 -0300

    Fix the divide by zero error.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -29,6 +29,11 @@ switch (Console.ReadLine())
         Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
         break;
     case "d":
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
         break;
 }
